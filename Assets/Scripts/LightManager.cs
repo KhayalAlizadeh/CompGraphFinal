bd3d94b78e0ccada00e2d91598362f44d2e272de
KhayalAlizadeh <79930118+KhayalAlizadeh@users.noreply.github.com>
2025-11-26 23:14:36 +0400

    change

--- Assets/Scripts/LightManager.cs
+++ Assets/Scripts/LightManager.cs
@@ -1,5 +1,30 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LightManager : MonoBehaviour {
+    enum Light {
+        Green,
+        Red,
+        Yellow
+    }
+    [SerializeField] private Light activeLightState = Light.Green;
+    private void Start() {
+        StartCoroutine(DynamicLight());
+    }
+
+    private IEnumerator DynamicLight() {
+        int duration;
+
+        while (true) {
+            duration = Random.Range(2, 6);
+            int index = (int)activeLightState;
+            int target = (index + 1) % 3;
+            activeLightState = (Light)target;
+
+            yield return new WaitForSeconds(duration);
+        }
+    }
 
 }
